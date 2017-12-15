caa978dd110fea9c557834b8997046ed1fe7ddb2
sravuri-nisum-com <30477858+sravuri-nisum-com@users.noreply.github.com>
2017-12-14 18:28:42 -0800

    Update Compare.js

--- Closures/Compare.js
+++ Closures/Compare.js
@@ -1,66 +1,48 @@
-function MyObject(name, message) {
-    
-         this.name = name.toString();
-    
-         this.message = message.toString();
-    
-    }
-    
-    MyObject.prototype.getName = function() {
-    
-         return this.name;
-    
+function multiplier ( factor ) {
+
+    return function ( number ) {
+
+       return number * factor ;
+
     };
-    
-    MyObject.prototype.getMessage = function() {
-    
-         return this.message;
-    
-    };
-    // ƒ () {
-    
-    //      return this.message;
-    
-    // }
-    function MyObject(name, message) {
-    
-            this.name = name.toString();
-    
-            this.message = message.toString();
-    
-    }
-    
-    MyObject.prototype = {
-    
-           getName: function() {
-    
-               return this.name;
-    
-           },
-    
-          getMessage: function() {
-    
-               return this.message;
-    
-          }
-    
-    }
-    //fdjjhksfhkjgjkwksj
-    function MyObject(name, message) {
-    
-         this.name = name.toString();
-    
-         this.message = message.toString();
-    
-         this.getName = function() {
-    
-               return this.name;
-    
-         };
-    
-         this.getMessage = function() {
-    
-              return this.message;
-    
-         };
-        }    
+
+}
+
+let twice = multiplier (2) ;
+
+console . log ( twice (5) ) ; // 10
+// in the above example the inner function can access the parameter factor of the outer function. 
+// It means it remembers the environment in which the inner function was created.
+
+// The behaviour of the closures frees you from having to worry about the lifetimes of 
+// variables and also allows for the creative use of function values.
+
+// One the major drawback of using closures is that, it consumes more memory and 
+// if not used properly can lead to ‘memory leaks’.
+function parent() {
+
+   let num = 5;
+
+   function child() {
+
+       return num;
+
+   }
+
+   return child;
+
+}
+
+let result = parent();
+
+console.log(result()); // 5
+
+//here in above example When the function parent is called, the local variable num is created and is assigned a value.
+// The parent function returns the function named child.
+
+// It is important to note that, the local variable num of the outer function(parent) 
+// exists even after the outer function has returned, as the inner function child holds the reference to the outer function variable num, so the variable num will not be garbage collected. Whereas in regular functions, when the function returns, its local variables no longer exists.
+
+// In such case, we need to release the memory explicitly by assigning null to the variables.
+
+// result = null;
